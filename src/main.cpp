bbf8cd3a12d09ee3b5d5fcb6d781c7a341a791c3
Robert <rgulyas01@gmail.com>
2025-12-29 15:14:21 +0100

    initial commit: cmake setup

---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+
+using namespace std;
+int main() {
+    cout << "hellow news" << endl;
+
+    return 0;
+}
---
+++ tests/test_newservice.cpp
@@ -0,0 +1,5 @@
+#include <gtest/gtest.h>
+
+TEST(HelloTest, WorksCorrectly) {
+    EXPECT_EQ(0, 0);
+}
