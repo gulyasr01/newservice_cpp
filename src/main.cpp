1f980ec8c47c4b007c6c1742595135b225d5046a
Robert <rgulyas01@gmail.com>
2025-12-30 13:04:48 +0100

    class completeb but testing is not implemented

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,8 @@
 #include <unordered_map>
 #include <vector>
 #include <algorithm>
+#include <numeric>
+#include <set>
 
 using namespace std;
 
@@ -18,7 +20,11 @@ struct News {
     vector<string> topics;
 };
 
-
+struct News_metadata {
+    float timestamp;
+    unsigned int interest;
+    unsigned int id;
+};
 
 class NewsAggregator {
 public:
@@ -27,14 +33,16 @@ public:
     bool AddSubscription(unsigned int id, unsigned int minInterest, unsigned int maxNewsPerSecond, vector<string> && topics) {
         Subscriber sub {minInterest, maxNewsPerSecond, move(topics)};
         // create a map of news assigned to this subsciber - sorted by timestamp
-        subnews_.insert_or_assign(id, map<float, unsigned int>());
+        subnews_.insert_or_assign(id, multimap<float, News_metadata>());
         for(const auto & t : sub.topics) {
             topics_subs_[t].push_back(id); // bookeeping this user belongs to this topic
             // check if we have existing news in this topic, if yes, assign them to this user if interested
             if (topics_news_.contains(t)) {
                 for (auto news_id : topics_news_.at(t)) {
-                    if (news_.at(news_id).interest >= sub.minInterest) {
-                        subnews_.at(id).insert({news_.at(news_id).timestamp, news_id});
+                    const News & n = news_.at(news_id);
+                    if (n.interest >= sub.minInterest) {
+                        News_metadata md{n.timestamp, n.interest, news_id};
+                        subnews_.at(id).insert({news_.at(news_id).timestamp, move(md)});
                     }
                 }
             }
@@ -57,7 +65,8 @@ public:
             // assings this new to the users who are interested in it
             for (const auto & sub_id : topics_subs_[t]) {
                 if (subs_[sub_id].minInterest >= news.interest) {
-                    subnews_[sub_id].insert({news.timestamp, id});
+                    News_metadata md{news.timestamp, news.interest, id};
+                    subnews_[sub_id].insert({news.timestamp, move(md)});
                 }
             }
         }
@@ -66,22 +75,50 @@ public:
         return true;
     }
 
-    void Publish(float timestamp, float maxAge) {
+    map<unsigned int, set<unsigned int>> Publish(float timestamp, float maxAge) {
         float start_t = timestamp - maxAge;
         float end_t = timestamp;
 
-        vector<pair<unsigned int, vector<unsigned>>> sub_news_filtered;
+        map<unsigned int, set<unsigned int>> news_to_users; 
+
+        // generate the news for every user
+        vector<pair<unsigned int, vector<unsigned>>> sub_news_filtered; // vec<(sub_id, vec<news_id>)>, todo: storing this is unnecessary
         for (const auto & subnews : subnews_) {
-            vector<unsigned int> news_in_range;
+            // get the metadatas of the news in the given time range
+            vector<News_metadata> news_in_range;
             auto itStart = subnews.second.lower_bound(start_t);
             auto itEnd = subnews.second.lower_bound(end_t);
             for (auto it = itStart; it != itEnd; ++it) {
                 news_in_range.push_back(it->second);
             }
-            if (news_in_range.size() <= subs_[subnews.first].maxNewsPerSecond) {
-                sub_news_filtered.push_back({subnews.first, news_in_range});
+
+            const Subscriber & sub = subs_[subnews.first];
+            vector<unsigned> filtered_news_ids(sub.maxNewsPerSecond);
+            // all the news in the time range can be displayed
+            if (news_in_range.size() <= sub.maxNewsPerSecond) {
+                for (const auto & md : news_in_range) {
+                    filtered_news_ids.push_back(md.id);
+                    news_to_users[md.id].insert(subnews.first); // converting: user gets new -> new gets this user
+                }
+            // news need to be sorted
+            } else {
+                vector<unsigned> filtered_idxs_desc(news_in_range.size());
+                iota(filtered_idxs_desc.begin(), filtered_idxs_desc.end(), 0); // indexes
+                sort(filtered_idxs_desc.begin(), filtered_idxs_desc.end(), [&](int a, int b){
+                    return tie(news_in_range[a].interest, news_in_range[a].timestamp, news_in_range[a].timestamp) > 
+                           tie(news_in_range[b].interest, news_in_range[b].timestamp, news_in_range[b].timestamp);
+                });
+                
+                for (int i = 0; i < filtered_news_ids.size(); ++i) {
+                    filtered_news_ids[i] = news_in_range[filtered_idxs_desc[i]].id;
+                    news_to_users[news_in_range[filtered_idxs_desc[i]].id].insert(subnews.first); // converting: user gets new -> new gets this user
+                }
             }
+            sub_news_filtered.push_back({subnews.first, move(filtered_news_ids)});
         }
+
+        // convert the user:[news] to news:[user]
+        return news_to_users;
     }
 
 private:
@@ -89,7 +126,7 @@ private:
     unordered_map<unsigned int, News> news_;
     unordered_map<string, vector<unsigned int>> topics_news_; // bookeeping for news assigned to topics
     unordered_map<string, vector<unsigned int>> topics_subs_; // bookeeping for subs assigned to topics
-    unordered_map<unsigned int, map<float, unsigned int>> subnews_;
+    unordered_map<unsigned int, multimap<float, News_metadata>> subnews_;
 };
 
 
