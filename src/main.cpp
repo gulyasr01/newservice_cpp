4c8cffc96136b40e8ea6148d517b64db63c788a5
Robert <rgulyas01@gmail.com>
2025-12-31 11:30:46 +0100

    2nd test implemented but fails because of logic bug in sorting in publish

--- src/main.cpp
+++ src/main.cpp
@@ -151,5 +151,21 @@ int main() {
     map<unsigned int, set<unsigned int>> pubs = nag.Publish(200.0f, 100.0f);
     print(pubs);
 
+    cout << endl;
+
+    NewsAggregator nag2;
+    nag.AddSubscription(1, 5, 2, vector<string>{"technology"});
+    nag.AddSubscription(2, 5, 2, vector<string>{"technology", "sport"});
+    nag.AddSubscription(3, 1, 100, vector<string>{"sport"});
+    nag.NewsReceived(1, 10.0f, 4, vector<string>{"sport"});
+    nag.NewsReceived(2, 20.0f, 4, vector<string>{"technology"});
+    nag.NewsReceived(3, 30.0f, 5, vector<string>{"sport"});
+    nag.NewsReceived(4, 40.0f, 5, vector<string>{"technology"});
+    nag.NewsReceived(5, 50.0f, 6, vector<string>{"sport", "technology"});
+    nag.NewsReceived(6, 1003.0f, 5, vector<string>{"technology"});
+    nag.NewsReceived(7, 1004.0f, 5, vector<string>{"technology", "sport"});
+    map<unsigned int, set<unsigned int>> pubs2 = nag.Publish(1020.0f, 1000.0f);
+    print(pubs2);
+
     return 0;
 }
