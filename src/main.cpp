08979de055bd52fe87cc4c037aaed00454ca6026
Robert <rgulyas01@gmail.com>
2025-12-31 17:34:58 +0100

    minor naming refactor

--- src/main.cpp
+++ src/main.cpp
@@ -33,16 +33,16 @@ public:
     bool AddSubscription(unsigned int id, unsigned int minInterest, unsigned int maxNewsPerSecond, vector<string> && topics) {
         Subscriber sub {minInterest, maxNewsPerSecond, move(topics)};
         // create a map of news assigned to this subsciber - sorted by timestamp
-        subnews_.insert_or_assign(id, multimap<float, News_metadata>());
+        sub_news_.insert_or_assign(id, multimap<float, News_metadata>());
         for(const auto & t : sub.topics) {
-            topics_subs_[t].push_back(id); // bookeeping this user belongs to this topic
+            topic_subs_[t].push_back(id); // bookeeping this user belongs to this topic
             // check if we have existing news in this topic, if yes, assign them to this user if interested
-            if (topics_news_.contains(t)) {
-                for (auto news_id : topics_news_.at(t)) {
+            if (topic_news_.contains(t)) {
+                for (auto news_id : topic_news_.at(t)) {
                     const News & n = news_.at(news_id);
                     if (n.interest >= sub.minInterest) {
                         News_metadata md{n.timestamp, n.interest, news_id};
-                        subnews_.at(id).insert({news_.at(news_id).timestamp, move(md)});
+                        sub_news_.at(id).insert({news_.at(news_id).timestamp, move(md)});
                     }
                 }
             }
@@ -61,20 +61,20 @@ public:
         News news {timestamp, interest, move(topics)};
 
         for (const auto & t : news.topics) {
-            topics_news_[t].push_back(id); // bookeeping this news belongs to these topics
+            topic_news_[t].push_back(id); // bookeeping this news belongs to these topics
             // assings this new to the users who are interested in it
-            for (const auto & sub_id : topics_subs_[t]) {
+            for (const auto & sub_id : topic_subs_[t]) {
                 if (subs_[sub_id].minInterest <= news.interest) {
                     News_metadata md{news.timestamp, news.interest, id};
                     // check if this key-val already exists (this new was alreaddy added by another topic)
-                    auto range = subnews_[sub_id].equal_range(news.timestamp);
+                    auto range = sub_news_[sub_id].equal_range(news.timestamp);
                     bool exists = std::any_of(range.first, range.second,
                         [&](auto const& elem) {
                             return (elem.second.id == md.id);
                         });
 
                     if (!exists) {
-                        subnews_[sub_id].insert({news.timestamp ,move(md)});
+                        sub_news_[sub_id].insert({news.timestamp ,move(md)});
                     }
                 }
             }
@@ -90,7 +90,7 @@ public:
 
         map<unsigned int, set<unsigned int>> news_to_users; 
 
-        for (const auto & subnews : subnews_) {
+        for (const auto & subnews : sub_news_) {
             // get the metadatas of the news in the given time range
             vector<const News_metadata *> news_in_range;
             auto itStart = subnews.second.lower_bound(start_t);
@@ -129,9 +129,9 @@ public:
 private:
     unordered_map<unsigned int, Subscriber> subs_;
     unordered_map<unsigned int, News> news_;
-    unordered_map<string, vector<unsigned int>> topics_news_; // bookeeping for news assigned to topics
-    unordered_map<string, vector<unsigned int>> topics_subs_; // bookeeping for subs assigned to topics
-    unordered_map<unsigned int, multimap<float, News_metadata>> subnews_; // todo: maybe just use multimat<ts, newsid> and deprecate news_metadata
+    unordered_map<string, vector<unsigned int>> topic_news_; // bookeeping for news assigned to topics
+    unordered_map<string, vector<unsigned int>> topic_subs_; // bookeeping for subs assigned to topics
+    unordered_map<unsigned int, multimap<float, News_metadata>> sub_news_; // todo: maybe just use multimat<ts, newsid> and deprecate news_metadata
 };
 
 void print_publish(const std::map<unsigned int, std::set<unsigned int>>& m) {
